f9fd750d03902ebc14940ba7451ac356e3f6e3b6
agorapilled <johngardner13@outlook.com>
2024-10-18 10:20:18 -0400

    v0

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -143,18 +143,6 @@ const Chatbot = () => {
           <div className="flex justify-between items-center">
             <div className='flex justify-center items-center space-x-2'>
                 <div className="font-mono text-lg">GOVAI</div>
-                <div className='flex justify-center items-center z-10'>
-                  <a href="https://x.com/pigeontechgovai" className=''>
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-4 md:size-6 md:hover:scale-105 transition ease-in-out duration-150' fill="#00000" viewBox="0 0 50 50">
-                      <path d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"></path>
-                    </svg>
-                  </a>
-                  <a href="https://t.me/+MdDumaNLRblhY2U6" className=''>
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-4 md:size-6 md:hover:scale-105 transition ease-in-out duration-150' fill="#29A0DA" viewBox="0 0 50 50">
-                      <path d="M46.137,6.552c-0.75-0.636-1.928-0.727-3.146-0.238l-0.002,0C41.708,6.828,6.728,21.832,5.304,22.445	c-0.259,0.09-2.521,0.934-2.288,2.814c0.208,1.695,2.026,2.397,2.248,2.478l8.893,3.045c0.59,1.964,2.765,9.21,3.246,10.758	c0.3,0.965,0.789,2.233,1.646,2.494c0.752,0.29,1.5,0.025,1.984-0.355l5.437-5.043l8.777,6.845l0.209,0.125	c0.596,0.264,1.167,0.396,1.712,0.396c0.421,0,0.825-0.079,1.211-0.237c1.315-0.54,1.841-1.793,1.896-1.935l6.556-34.077	C47.231,7.933,46.675,7.007,46.137,6.552z M22,32l-3,8l-3-10l23-17L22,32z"></path>
-                    </svg>
-                  </a>
-                </div>
             </div>
             <button onClick={closeChat} className="text-gray-600 hover:text-red-500 text-lg">
               X
